3c5bb0b4481ff7852ed4cd0b26464b2374b3ea99
na2kera <nat3mitaro@gmail.com>
2025-02-22 12:33:02 +0900

    別ファイル切り出し

--- app.vue
+++ app.vue
@@ -30,36 +30,5 @@
 </template>
 
 <script setup lang="ts">
-const projects = [
-  {
-    id: 1,
-    title: "プロジェクト1",
-    date: "2025.1.23",
-    description: "プロジェクトの説明文がここに入ります",
-    thumbnail: "/images/projects/3AI.webp",
-    images: ["/images/projects/3AI.webp", "/images/projects/3AI-2.png"],
-    urls: [
-      "https://github.com/Akira0809/vol.10",
-      "https://github.com/Akira0809/vol.10-2023",
-      "https://github.com/na2kera/likeChatgptFront",
-    ],
-    slide:
-      "https://www.canva.com/design/DAFw9HktVmM/XQi_fhjgtZfzbjz9k8oa8A/view?utm_content=DAFw9HktVmM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0ba2a0468c",
-  },
-  {
-    id: 2,
-    title: "プロジェクト2",
-    date: "2025.2.10",
-    description: "プロジェクトの説明文がここに入ります",
-    thumbnail: "/images/projects/refty.png",
-    images: [
-      "/images/projects/refty.png",
-      "/images/projects/refty.png",
-      "/images/projects/refty.png",
-      "/images/projects/refty.png",
-    ],
-    urls: ["https://github.com/yusei53/refty"],
-    slide: "",
-  },
-];
+import { projects } from "~/data/projects";
 </script>
---
+++ data/projects.ts
@@ -0,0 +1,44 @@
+export interface Project {
+  id: number;
+  title: string;
+  date: string;
+  description: string;
+  thumbnail: string;
+  images: string[];
+  urls?: string[];
+  movie?: string;
+  slide?: string;
+}
+
+export const projects: Project[] = [
+  {
+    id: 1,
+    title: "プロジェクト1",
+    date: "2025.1.23",
+    description: "プロジェクトの説明文がここに入ります",
+    thumbnail: "/images/projects/3AI.webp",
+    images: ["/images/projects/3AI.webp", "/images/projects/3AI-2.png"],
+    urls: [
+      "https://github.com/Akira0809/vol.10",
+      "https://github.com/Akira0809/vol.10-2023",
+      "https://github.com/na2kera/likeChatgptFront",
+    ],
+    slide:
+      "https://www.canva.com/design/DAFw9HktVmM/XQi_fhjgtZfzbjz9k8oa8A/view?utm_content=DAFw9HktVmM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0ba2a0468c",
+  },
+  {
+    id: 2,
+    title: "プロジェクト2",
+    date: "2025.2.10",
+    description: "プロジェクトの説明文がここに入ります",
+    thumbnail: "/images/projects/refty.png",
+    images: [
+      "/images/projects/refty.png",
+      "/images/projects/refty.png",
+      "/images/projects/refty.png",
+      "/images/projects/refty.png",
+    ],
+    urls: ["https://github.com/yusei53/refty"],
+    slide: "",
+  },
+];
